466a3f52927e2cdb701a9cf79e3f9e35eab7de1d
Edward Yose <youngedward472@gmail.com>
2020-01-16 10:43:59 +0700

    *

--- app/src/main/java/com/example/android/SplashScreen.java
+++ app/src/main/java/com/example/android/SplashScreen.java
@@ -7,16 +7,17 @@ import android.os.Handler;
 import android.view.Window;
 
 public class SplashScreen extends AppCompatActivity {
-    private static int SPLASH_DISPLAY_LENGTH = 4000;
+    private int SPLASH_DISPLAY_LENGTH = 4000;
 
     @Override
-    protected void onCreate(Bundle savedsplash) {
-        super.onCreate(savedsplash);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.splash_screen);
 
-        final Handler handler = new Handler();
-        handler.postDelayed(new Runnable(){
+        new Handler().postDelayed(new Runnable(){
             @Override
             public void run(){
                 startActivity(new Intent(getApplicationContext(), MainActivity.class));
